8d29ffffa3620e56477e6051e66f5f1127d6ea11
Rabahjamal <rabah9686@gmail.com>
2019-12-28 11:46:32 +0200

    implemented messages view feature (up to a maximum of 100 messages)

--- application.py
+++ application.py
@@ -1,8 +1,9 @@
 import os
 
-from flask import Flask, request, render_template, redirect, url_for, flash
+from flask import Flask, request, render_template, redirect, url_for, flash, abort
 from flask_socketio import SocketIO, emit, send
 from flask_session import Session
+import jsons
 from message import *
 
 app = Flask(__name__)
@@ -25,7 +26,10 @@ def home():
 
 @app.route("/channel/<string:channel_name>")
 def channel(channel_name):
-    return render_template("channel.html", channel_name=channel_name)
+    if channel_name in channels:
+        return render_template("channel.html", channel_name=channel_name)
+    else:
+        return abort(404)
 
 @app.route("/new_channel", methods=["POST"])
 def new_channel():
@@ -35,16 +39,27 @@ def new_channel():
         flash("This channel is already exist!")
         return redirect(url_for("home"))
     else:
-        channels[channel_name] = Message(None, None, None)
+        channels[channel_name] = [jsons.dump(Message(None, None, None, None))]
         return redirect(url_for("home"))
 
 @socketio.on('new message')
 def handle_message(data):
     message = data["message_text"]
-    print(message)
-    emit("messages", message, broadcast=True)
+    sender = data["sender"]
+    channel_name = data["channel_name"]
+    #channels[channel_name].append(Message(sender, message, None))
+    channels[channel_name].append(jsons.dump(Message(sender, message, None, channel_name)))
+    if len(channels[channel_name]) == 101:
+        del channels[channel_name][1]
+    print(message + ': ' + sender + ', ' + channel_name)
+    emit("messages", jsons.dump(Message(sender, message, None, channel_name)), broadcast=True)
     #print('Message: ' + data)
     #send(data, broadcast=True)
+
+@socketio.on('new connection')
+def handle_connection(data):
+    channel_name = data["channel_name"];
+    emit("connection", channels[channel_name], broadcast=False)
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
--- message.py
+++ message.py
@@ -2,7 +2,8 @@
 class Message:
     """docstring for Channel."""
 
-    def __init__(self, sender, text, date):
+    def __init__(self, sender, text, date, channel_name):
         self.sender = sender
         self.text = text
         self.date = date
+        self.channel_name = channel_name
